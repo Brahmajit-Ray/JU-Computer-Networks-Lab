c9ad7caea5e073a58b31d96af896d32fd8f6670a
Brahmajit-Ray <115690721+Brahmajit-Ray@users.noreply.github.com>
2024-04-22 17:03:50 +0530

    Stop_N_Wait ARQ Noiseless

---
+++ Stop_N_Wait_Noiseless.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum{frame_arrival} event_type;
+typedef enum{data,acknowledgement} frame_kind;
+
+int seq_no=1;
+
+typedef struct{
+	frame_kind kind;
+	int seq;
+	int ack;
+	int info;
+}frame;
+
+frame frame_through_channel;
+
+event_type global_event=frame_arrival;
+
+void wait_for_event(event_type event){            //If frames available,then proceed
+	while(global_event!=frame_arrival){
+	
+	}
+	event=frame_arrival;
+}
+
+int get_data(){
+	int d;
+	printf("Enter packet data to be sent:");      //Get some data for the frame
+	scanf("%d",&d);
+	return d;
+}
+
+frame make_frame(frame_kind kind,int info,int ack){   //Get data from the user for the frame
+	frame f;
+	f.kind=kind;
+	
+	if(f.kind==data){
+		f.seq=seq_no;
+		seq_no++;
+	}
+	
+	f.ack=ack;
+	f.info=info;
+	
+	return f;	
+}
+
+void put_frame_on_channel(frame f){
+	int receiver();
+	frame_through_channel=f;
+	if(f.kind==data){
+		receiver();
+	}
+}
+
+void send_frame(frame f){
+	put_frame_on_channel(f);
+}
+
+
+frame receive_frame(){
+	frame recv=frame_through_channel;
+	return recv;
+}
+
+
+int extract_data(frame f){
+	return f.info;
+}
+
+void sender(){
+	int frame_data;
+	frame generated;
+	
+	frame_data=get_data();
+	generated=make_frame(data,frame_data,0);
+	send_frame(generated);
+	
+	frame ack_frame;
+	ack_frame=receive_frame();
+	if(ack_frame.kind==acknowledgement){
+		printf("Ack Frame received for packet-number %d\n",ack_frame.ack);
+	}
+}
+
+
+int receiver(){
+	event_type event;
+	frame received;
+	int extracted_data;
+	int ack_no;
+	
+	wait_for_event(event);
+	received=receive_frame();
+	extracted_data=extract_data(received);
+	ack_no=received.seq;
+	printf("Data extracted from the sent frame:%d\n",extracted_data);
+	
+	frame ack_frame=make_frame(acknowledgement,0,ack_no);
+	send_frame(ack_frame);
+}
+
+int main(){
+	int track=0;
+	printf("Stop_N_Wait ARQ(Noiseless) Simulator for upto 5 packets\n");
+	while(track<5){
+		sender();
+		track++;
+	}
+}
+
+
